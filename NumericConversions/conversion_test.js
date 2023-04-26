87132ac1b03ab2075907f194798278571b513237
Dillon Coe <SupersonicC123@gmail.com>
2023-04-26 09:32:37 -0400

    added conversion script

---
+++ NumericConversions/conversion_test.js
@@ -0,0 +1,24 @@
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+console.log(a = parseInt(a))
+console.log(b = parseInt(b))
+console.log(c = parseInt(c))
+console.log(d = parseInt(d))
+
+console.log(a = parseFloat(a))
+console.log(b = parseFloat(b))
+console.log(c = parseFloat(c))
+console.log(d = parseFloat(d))
+
+console.log(a = Number(a))
+console.log(b = Number(b))
+console.log(c = Number(c))
+console.log(d = Number(d))
+
+console.log(+a)
+console.log(+b)
+console.log(+c)
+console.log(+d)
